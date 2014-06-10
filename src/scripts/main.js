e8c9e4a86167a5cc0aef274f30d12fc84a7731a9
Renato Massao Yonamine <rmy.yonamine@gmail.com>
2014-06-10 15:51:09 -0300

    added drawing to canvas

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
 	var socket = io();
+	var el = document.getElementById('myCanvas');
+	var ctx = el.getContext('2d');
+	var isDrawing;
+
+	el.onmousedown = function(e) {
+		isDrawing = true;
+		ctx.lineWidth = 10;
+		ctx.lineJoin = ctx.lineCap = 'round';
+		ctx.moveTo(e.layerX, e.layerY);
+	};
+	el.onmousemove = function(e) {
+		if (isDrawing) {
+			ctx.lineTo(e.layerX, e.layerY);
+			ctx.stroke();
+		}
+	};
+	el.onmouseup = function() {
+		isDrawing = false;
+	};
 });
